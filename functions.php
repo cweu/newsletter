624de0f27b6a6e72e41da96d47ccd3d98f541a1c
wvengen <dev-wordpress@willem.engen.nl>
2017-12-27 18:55:33 +0100

    Remove right sidebar

--- functions.php
+++ functions.php
@@ -153,16 +153,6 @@ add_action( 'after_setup_theme', 'twentysixteen_content_width', 0 );
  */
 function twentysixteen_widgets_init() {
 	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'twentysixteen' ),
-		'id'            => 'sidebar-1',
-		'description'   => __( 'Add widgets here to appear in your sidebar.', 'twentysixteen' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-
-	register_sidebar( array(
 		'name'          => __( 'Content Bottom 1', 'twentysixteen' ),
 		'id'            => 'sidebar-2',
 		'description'   => __( 'Appears at the bottom of the content on posts and pages.', 'twentysixteen' ),
@@ -304,11 +294,6 @@ function twentysixteen_body_classes( $classes ) {
 	// Adds a class of group-blog to sites with more than 1 published author.
 	if ( is_multi_author() ) {
 		$classes[] = 'group-blog';
-	}
-
-	// Adds a class of no-sidebar to sites without active sidebar.
-	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-		$classes[] = 'no-sidebar';
 	}
 
 	// Adds a class of hfeed to non-singular pages.
--- image.php
+++ image.php
@@ -108,5 +108,4 @@ get_header(); ?>
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
