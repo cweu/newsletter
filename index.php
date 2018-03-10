e724f36089b7074d58209a8c7c27ba32453f922a
wvengen <dev-wordpress@willem.engen.nl>
2018-03-10 11:15:53 +0100

    Fix article list on frontpage

--- index.php
+++ index.php
@@ -37,19 +37,24 @@ get_header(); ?>
 				<?php endif; ?>
 			<?php endif; ?>
 
+			<div class="the-content">
+				<?php
+				// Start the loop.
+				while ( have_posts() ) : the_post();
+
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', get_post_format() ?: get_post_type() );
+
+				// End the loop.
+				endwhile;
+				?>
+			</div>
+
 			<?php
-			// Start the loop.
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() ?: get_post_type() );
-
-			// End the loop.
-			endwhile;
 
 			// Previous/next page navigation.
 			the_posts_pagination( array(
