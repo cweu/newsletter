382b37afb50cc9c0a123ba64d4858a48329ebf39
wvengen <dev-wordpress@willem.engen.nl>
2018-03-10 11:20:19 +0100

    Fix pagination in newsletter article archive

--- archive.php
+++ archive.php
@@ -31,19 +31,23 @@ get_header(); ?>
 			</header><!-- .page-header -->
 
 			<div class="the-content">
+				<?php
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
+
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', get_post_format() ?: get_post_type() );
+
+				// End the loop.
+				endwhile;
+				?>
+			</div>
+
 			<?php
-			// Start the Loop.
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() ?: get_post_type() );
-
-			// End the loop.
-			endwhile;
 
 			// Previous/next page navigation.
 			the_posts_pagination( array(
@@ -58,7 +62,6 @@ get_header(); ?>
 
 		endif;
 		?>
-		</div>
 
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
